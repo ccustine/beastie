9ae6237d9a5cb63232ae910391e18721bf36a27e
Chris Custine <ccustine@apache.org>
2018-12-04 18:36:23 -0700

    Add metrics to REST API
    Fix JSON formatting
    General cleanup of feed API
    
    Signed-off-by: Chris Custine <ccustine@apache.org>

--- output/jsonapi.go
+++ output/jsonapi.go
@@ -16,12 +16,13 @@ package output
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/ccustine/beastie/types"
 	"github.com/gorilla/mux"
-	log "github.com/sirupsen/logrus"
-	"net"
+	"github.com/rcrowley/go-metrics"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -30,14 +31,20 @@ const (
 )
 
 type JsonOutput struct {
-	knownAircraft types.AircraftMap
+	aircraftList []*types.AircraftData
 }
+
+var (
+	aircraftList []*types.AircraftData
+	lock sync.RWMutex
+)
 
 func NewJsonOutput() *JsonOutput {
 	jsonApi := &JsonOutput{}
 
 	r := mux.NewRouter()
 	r.HandleFunc("/feed", jsonApi.FeedHandler)
+	r.HandleFunc("/metrics", jsonApi.MetricsHandler)
 
 	// This will serve files under http://localhost:8000/static/<filename>
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("/tmp"))))
@@ -48,17 +55,19 @@ func NewJsonOutput() *JsonOutput {
 
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "127.0.0.1:8000",
+		Addr:         "0.0.0.0:8000",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-	listener, err := net.ListenTCP("tcp4", &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 8000})
-	if err != nil {
-		log.Fatal("error creating listener")
-	}
+	//listener, err := net.ListenTCP("tcp4", &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 8000})
+	//if err != nil {
+	//	log.Fatal("error creating listener")
+	//}
 
-	go srv.Serve(listener)
+	//go logrus.Error(srv.ListenAndServe())
+	//go srv.Serve(listener)
+	go srv.ListenAndServe()
 	//go log.Fatal(srv.ListenAndServe())
 	//go log.Fatal(srv.ListenAndServe())
 
@@ -66,25 +75,30 @@ func NewJsonOutput() *JsonOutput {
 }
 
 func (o JsonOutput) UpdateDisplay(knownAircraft *types.AircraftMap) {
-	o.knownAircraft = *knownAircraft
+	//sortedAircraft := make(AircraftList, 0, aircraftList.Len())
+
+	lock.Lock()
+	aircraftList = knownAircraft.Copy()
+	lock.Unlock()
+
 	//var b strings.Builder
 
-	//sortedAircraft := make(AircraftList, 0, knownAircraft.Len())
+	//sortedAircraft := make(AircraftList, 0, aircraftList.Len())
 
-	//for _, aircraft := range knownAircraft.Copy() {
+	//for _, aircraft := range aircraftList.Copy() {
 	//	sortedAircraft = append(sortedAircraft, aircraft)
 	//}
 
 	//sort.Sort(sortedAircraft)
 
-	//for _, aircraft := range knownAircraft.Copy() {
+	//for _, aircraft := range aircraftList.Copy() {
 	//	evict := time.Since(aircraft.LastPing) > (time.Duration(59) * time.Second)
 	//
 	//	if evict {
 	//		//if o.Beastinfo.Debug {
 	//		//	log.Debugf("Evicting %d", aircraft.IcaoAddr)
 	//		//}
-	//		knownAircraft.Delete(aircraft.IcaoAddr)
+	//		aircraftList.Delete(aircraft.IcaoAddr)
 	//		continue
 	//	}
 	//
@@ -97,8 +111,27 @@ func (o JsonOutput) UpdateDisplay(knownAircraft *types.AircraftMap) {
 //func (a *App) FeedHandler(w http.ResponseWriter, r *http.Request) {
 func (o JsonOutput) FeedHandler(w http.ResponseWriter, r *http.Request) {
 	//respondWithJSON(w, http.StatusOK, products)
+	lock.RLock()
+	respondWithJSON(w, http.StatusOK, aircraftList)
+	lock.RUnlock()
+}
 
-	respondWithJSON(w, http.StatusOK, o.knownAircraft)
+func (o JsonOutput) MetricsHandler(w http.ResponseWriter, r *http.Request) {
+	var b strings.Builder
+
+	goodRate := metrics.GetOrRegisterMeter("Message Rate (Good)", metrics.DefaultRegistry)
+	badRate := metrics.GetOrRegisterMeter("Message Rate (Bad)", metrics.DefaultRegistry)
+	//ModeACCnt          := metrics.GetOrRegisterCounter("Message Rate (ModeA/C)", metrics.DefaultRegistry)
+	//ModesShortCnt      := metrics.GetOrRegisterCounter("Message Rate (ModeS Short)", metrics.DefaultRegistry)
+	//ModesLongCnt       := metrics.GetOrRegisterCounter("Message Rate (ModeS Long)", metrics.DefaultRegistry)
+
+	lock.RLock()
+	b.WriteString(fmt.Sprintf("{\"now\": %d,\"total\":%d,\"good\":%.1f,\"bad\":%.1f}", time.Now().Unix(), len(aircraftList), goodRate.Rate1(), badRate.Rate1()))
+	lock.RUnlock()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(b.String()))
 }
 
 func respondWithError(w http.ResponseWriter, code int, message string) {
@@ -107,15 +140,26 @@ func respondWithError(w http.ResponseWriter, code int, message string) {
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	var b strings.Builder
-	acm := payload.(*types.AircraftMap)
+	acl := payload.([]*types.AircraftData)
 
-	sortedAircraft := make(AircraftList, 0, acm.Len())
+	goodRate := metrics.GetOrRegisterMeter("Message Rate (Good)", metrics.DefaultRegistry)
+	badRate := metrics.GetOrRegisterMeter("Message Rate (Bad)", metrics.DefaultRegistry)
+	//ModeACCnt          := metrics.GetOrRegisterCounter("Message Rate (ModeA/C)", metrics.DefaultRegistry)
+	//ModesShortCnt      := metrics.GetOrRegisterCounter("Message Rate (ModeS Short)", metrics.DefaultRegistry)
+	//ModesLongCnt       := metrics.GetOrRegisterCounter("Message Rate (ModeS Long)", metrics.DefaultRegistry)
 
-	for _, aircraft := range acm.Copy() {
+	b.WriteString(fmt.Sprintf("{\"now\": %d,\"total\":%d,\"good\":%.1f,\"bad\":%.1f,\"aircraft\":[", time.Now().Unix(), len(acl), goodRate.Rate1(), badRate.Rate1()))
+	for i, aircraft := range acl {
 		acmb, _ := json.Marshal(aircraft)
 		b.Write(acmb)
-		sortedAircraft = append(sortedAircraft, aircraft)
+		if i <= len(acl) - 2 {
+			b.WriteString(",")
+		}
+
+		//sortedAircraft = append(sortedAircraft, aircraft)
 	}
+
+	b.WriteString("]}")
 
 
 	//response, _ := json.Marshal(sortedAircraft)
